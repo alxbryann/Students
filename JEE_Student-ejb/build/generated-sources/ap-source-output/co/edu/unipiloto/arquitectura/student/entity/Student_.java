6aea4f1d01e57fb668db7f7621351e7b54c9cd80
alxbryann <bryanalexanderbogota@gmail.com>
2024-09-09 16:59:09 -0500

    Second commit

---
+++ JEE_Student-ejb/build/generated-sources/ap-source-output/co/edu/unipiloto/arquitectura/student/entity/Student_.java
@@ -0,0 +1,16 @@
+package co.edu.unipiloto.arquitectura.student.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-09-09T16:26:35")
+@StaticMetamodel(Student.class)
+public class Student_ { 
+
+    public static volatile SingularAttribute<Student, Integer> studentid;
+    public static volatile SingularAttribute<Student, String> firstname;
+    public static volatile SingularAttribute<Student, Integer> yearlevel;
+    public static volatile SingularAttribute<Student, String> lastname;
+
+}
